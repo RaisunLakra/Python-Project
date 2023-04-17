20e4d8eec16991fb9ddd75b109314af8de62e06b
Raisun <raisunlakra18@gmail.com>
2023-04-17 16:15:26 +0530

    Added comment to line 32

--- project 1/project.py
+++ project 1/project.py
@@ -29,7 +29,7 @@ while True:
         user=input("Form valid word : ").upper()
 
         # check if user input matches the word
-        if Counter(word)==Counter(user) and user in selected_word:
+        if Counter(word)==Counter(user) and user in selected_word:     # Counter constructor takes and iterable and return a dictionary like object which that maps each unique element in the iterable to its frequency count
             print("correct")
             score+=1
         else:
